ae626b3a79d4fc2b48ff77c5164c5abe56d9eab9
Marcus Augusto <77218852+madogap@users.noreply.github.com>
2022-11-08 13:31:54 -0300

    .

--- Calcula/assest/indexcalcula.js
+++ Calcula/assest/indexcalcula.js
@@ -1,60 +1,54 @@
-function somar() {
-    let tn1 = window.document.getElementById('txtn1');
-    let tn2 = window.document.getElementById('txtn2');
-    let res = window.document.getElementById('res');
-    let n1 = Number(tn1.value);
-    let n2 = Number(tn2.value);
-    let s = n1 + n2;
-    res.innerHTML = `A soma entre ${n1} e ${n2} é igual a ${s}`;
-};
-function diminuir() {
-    let tn1 = window.document.querySelector('input#txtn1');
-    let tn2 = window.document.querySelector('input#txtn2');
-    let res = window.document.getElementById('res');
-    let n1 = Number(tn1.value);
-    let n2 = Number(tn2.value);
-    let s = n1 - n2;
-    res.innerHTML = `A soma entre ${n1} e ${n2} é igual a: ${s}`;
-};
-
-function multiplicar() {
-    let tn1 = window.document.getElementById('txtn1');
-    let tn2 = window.document.querySelector('input#txtn2');
-    let res = window.document.getElementById('res');
-    let n1 = Number(tn1.value);
-    let n2 = Number(tn2.value);
-    let s = n1 * n2;
-    res.innerHTML = `A multiplicação entre ${n1} e ${n2} é igual a: ${s}`;
-};
-
-function dividir() {
-    let tn1 = window.document.getElementById('txtn1');
-    let tn2 = window.document.querySelector('input#txtn2');
-    let res = window.document.getElementById('res');
-    let n1 = Number(tn1.value);
-    let n2 = Number(tn2.value);
-    let s = n1 / n2;
-    res.innerHTML = `A divisão entre ${n1} e ${n2} é igual a: ${s}`;
+const x = function multiplicar(a, b) {return a * b};
+let z = x(4, 1);
+//console.log(z);
+function somar(a, b, ...args) {
+   
+    return Number(a) + Number(b) + Number(args);
 }
-
-function ozero() {
-    let tn1 = window.document.getElementById('txtn1');
-    let tn2 = window.document.querySelector('input#txtn2');
-    let res = window.document.getElementById('res');
-    let n1 = Number(tn1.value);
-    let n2 = Number(tn2.value);
-    let s = n1 % n2;
-    res.innerHTML = `A divisão de resto zero entre ${n1} e ${n2} é igual a: ${s}`;
-
+console.log(somar(4, 1, 2, 3));
+function sub(a, b){
+    return parseFloat(a) - parseFloat(b);
 }
-
-function expoente() {
-    let tn1 = window.document.getElementById('txtn1');
-    let tn2 = window.document.querySelector('input#txtn2');
-    let res = window.document.getElementById('res');
-    let n1 = Number(tn1.value);
-    let n2 = Number(tn2.value);
-    let s = n1 ** n2;
-    res.innerHTML = `O expoencia entre ${n1} e ${n2} é igual a: ${s}`;
-
+class Doblo {
+    static doblo(n) {
+        n = n | 1;
+        return n * 2;
+    }
+} 
+class Tripple {
+    static tripple(n) {
+        n = n | 1;
+        return n * 3;
+    }
 }
+class BiggerDoblo extends Doblo {
+    static doblo(n) {
+        return super.doblo(n) * super.doblo(n);
+    }
+}
+class BiggerTripple extends Tripple {
+    static tripple(n) {
+        return super.tripple(n) * super.tripple(n);
+    }
+}
+//console.log(Doblo.doblo(1));
+//console.log(BiggerDoblo.doblo(3));
+console.log(Tripple.tripple(1));
+console.log(BiggerTripple.tripple(1));
+
+function multiply(multiplier, ...theArgs) {
+    return theArgs.map(function (element) {
+      return multiplier * element;
+    });
+  }
+  
+  var arr = multiply(3, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1);
+  console.log(arr); // [2, 4, 6]
+
+  function somar(somar, ...theArgs){
+    return theArgs.map(function(element){
+        return somar + element;
+    });
+  }
+  var ar = somar(2, 1, 2, 3, 4, 5); //[ 3, 4, 5, 6, 7 ]
+  console.log(ar);
