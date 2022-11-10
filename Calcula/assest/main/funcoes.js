f1283a58552298b5b3d458b19cf8d37db021d8a5
Marcus Augusto <77218852+madogap@users.noreply.github.com>
2022-11-10 15:09:56 -0300

    Adicionado funcoes main para por no JEST

---
+++ Calcula/assest/main/funcoes.js
@@ -0,0 +1,15 @@
+const num1 = 30;
+const num2 = 30;
+
+const soma = (a, b) => a + b
+const diminur = (a, b) => a - b
+const multipica = (a, b) => a * b
+const dividir = (a, b) => a / b
+
+const main = () => {
+    console.log(soma(num1, num2));
+    console.log(diminur(num1, num2));
+    console.log(multipica(num1, num2));
+    console.log(dividir(num1, num2));
+}
+main();
