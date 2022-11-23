ab2663e51dc2e10dc83f1e41fd5ce6637576d7b6
Marcus Augusto <77218852+madogap@users.noreply.github.com>
2022-11-23 11:34:54 -0300

    News

--- camp/main.js
+++ camp/main.js
@@ -1,4 +1,4 @@
 var myArray = [50, 60, 70];
 var myData = [50, 60, 70];
-var myData = myArray[0]
+var myData = myArray[0]//primeiro index
 console.log(myData);
---
+++ camp/matrixmulti.js
@@ -0,0 +1,20 @@
+const myArray = [[1, 2, 3], [4, 5, 6], [7, 8, 9], [[10, 11, 12], 13, 14],];
+const myData = myArray[0][0];
+console.log(myData);
+//Por no final
+myArray.push(["dog"])
+console.log(myArray);
+//Remover do final
+const removedFromMyArrayFinal = myArray.pop()
+console.log(removedFromMyArrayFinal);
+console.log(myArray);
+//Remover do inicio
+const removedFromMyArraInicio = myArray.shift();
+console.log(myArray);
+//Remover do inicio
+myArray.shift()
+//Por no inicio
+myArray.unshift(["Paul", 35]);
+console.log(myArray);
+var myList=[["cereal", 3], ["milk", 2], ["bananas", 3], ["juice", 2], ["eggs", 12]];
+console.log(myList);
