dbc3f0f185d05d35512a0c20f3104a19c0cc6e85
Geovanny L. Coutinho <geovanny.coutinho@victoriaplum.com>
2022-11-24 14:52:41 -0300

    New

--- camp/mr/NumberMethods/parseIntFloat.js
+++ camp/mr/NumberMethods/parseIntFloat.js
@@ -1,20 +1,29 @@
+
+
 /*
 
 parseInt()
 parseFloat()
 
 */
+var numb0 = "5" + 2; 
+console.log(numb0);// concatenando
+var numb =parseInt("5") + 2; //Somando
+console.log(numb);
+var numF =parseFloat("5.5") + 2;
+console.log(numF); //7.5
 var nu0 = parseInt("+21");
 var nu1 = parseInt("-21");
 var nu2 = parseInt("21.9");
 var nu3 = parseFloat("21.9");
 var nu4 = parseFloat("-21.9");
 var nu5 = parseFloat("21.9");
-var nu6 = parseFloat("Santy Seia")
+var nu6 = parseFloat("Santy Seia") //NaN
 console.log(nu0);
 console.log(nu1);
 console.log(nu2);
 console.log(nu3);
 console.log(nu4);
 console.log(nu5);
-console.log(nu6);
+console.log(nu6);//NaN
+
